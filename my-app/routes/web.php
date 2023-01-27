a338e94c371f22d90118a737352d1a57d88da278
sh-navid <shamshirsaz.navid@live.com>
2023-01-27 05:09:26 +0330

    Update

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -17,28 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return "<h1>Home Page</h1>";
-});
-
-Route::get('/about', function () {
-    return view("about");
-});
 
 Route::get('/test/{name}', function ($name) {
     return view("test");
 });
 
-Route::match(['get', 'post'], '/match', function () {
-    return "Route Match";
-});
-
-Route::any('/any', function () {
-    return "Route Any";
-});
-
-Route::redirect('/root', '/home', 301);
 
 Route::get('/csrf', function () {
     return view("csrf");
+});
+
+
+Route::post("/csrf-form", function () {
+    // $data = file_get_contents('php://input');
+    return "The value of input text is: ".$_POST["myText"];
 });
