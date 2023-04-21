af647c70e22bd1a184b1852e453bb6a6374506c5
sh-navid <shamshirsaz.navid@live.com>
2023-04-21 06:48:59 +0330

    Update

--- applications/api/README.md
+++ applications/api/README.md
@@ -1,6 +1,7 @@
 # API
 ## ReactJS
-- npx create-react-app wallet-front
-- 
+- `npx create-react-app wallet-front`
+    - `cd wallet-front`
+    - `npm start`
 ## Laravel
 - composer create-project laravel/laravel wallet-back
--- applications/api/wallet-front/src/App.js
+++ applications/api/wallet-front/src/App.js
@@ -1,8 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react"
 
-function App() {
-  return <div>hello</div>;
-}
+const Records = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useState(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/api/record/list")
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setData(res);
+          setLoading(false);
+        });
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      {isLoading == true ? (
+        <span>Loading ...</span>
+      ) : (
+        <div>
+          {data.answers.map((item, index) => (
+            <span key={index}>{item}</span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  return <Records/>;
+};
 
 export default App;
